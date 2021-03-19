70dee8cbf3190bfd754532de9c9afff727343ad0
Mike Centola <mcentola@appliedengdesign.com>
2021-03-19 17:21:16 -0400

    Fixed / Updated validation script

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* ---------------------------------------------------------------------------------------------
  *  Copyright (c) Applied Eng & Design All rights reserved.
@@ -8,79 +9,68 @@
 const start = process.hrtime();
 
 const Ajv = require('ajv').default;
+const path = require('path');
+const fs = require('fs');
 const schema = require('../src/json/schema/cnccodes.schema.json');
+
+const mtypes = ['Mill', 'Lathe', 'Printer'];
+const files = ['gcodes.json', 'mcodes.json'];
+const jpath = path.join('.', 'src', 'json/');
 
 process.stdout.write('Validating JSON...\n\n');
 
 // Load AJV
+process.stdout.write('Loading Schema...');
 const ajv = new Ajv({ allErrors: true });
 const validate = ajv.compile(schema);
+process.stdout.write('Done.\n\n');
 
-// Validate Mill G-Codes
-process.stdout.write('Mill G-Codes...');
-const millG = require('../src/json/mill/gcodes.json');
-let valid = validate(millG);
+mtypes.forEach(mt => {
+    process.stdout.write(`Checking ${mt} G-Codes...\n`);
+    try {
+        const file = fs.readFileSync(path.join(jpath, mt.toLowerCase(), files[0]), 'utf-8');
 
-if (valid) {
-    process.stdout.write('OK!\n\n');
-} else {
-    process.stderr.write(validate.errors);
-}
+        const g = JSON.parse(file);
 
-// Validate Mill M-Codes
-process.stdout.write('Mill M-Codes...');
-const millM = require('../src/json/mill/mcodes.json');
-valid = validate(millM);
+        const valid = validate(g);
 
-if (valid) {
-    process.stdout.write('OK!\n\n');
-} else {
-    process.stderr.write(validate.errors);
-}
+        if (valid) {
+            process.stdout.write('OK\n\n');
+        } else {
+            for (const err of validate.errors) {
+                process.stdout.write(`${err.dataPath} ${err.message ?? ''}\n`);
+            }
+            process.stdout.write(`-- ${mt} G-Codes has errors --\n\n`);
+        }
+    } catch (err) {
+        if (err instanceof Error && err.code === 'ENOENT') {
+            process.stderr.write(err.message);
+        }
+    }
 
-// Validate Lathe G-Codes
-process.stdout.write('Lathe G-Codes...');
-const latheG = require('../src/json/lathe/gcodes.json');
-valid = validate(latheG);
+    process.stdout.write(`Checking ${mt} M-Codes...\n`);
 
-if (valid) {
-    process.stdout.write('OK!\n\n');
-} else {
-    process.stderr.write(validate.errors);
-}
+    try {
+        const file = fs.readFileSync(path.join(jpath, mt.toLowerCase(), files[1]), 'utf-8');
 
-// Validate Lathe M-Codes
-process.stdout.write('Lathe M-Codes...');
-const latheM = require('../src/json/lathe/mcodes.json');
-valid = validate(latheM);
+        const m = JSON.parse(file);
 
-if (valid) {
-    process.stdout.write('OK!\n\n');
-} else {
-    process.stderr.write(validate.errors);
-}
+        const valid = validate(m);
 
-// Validate 3D Printer G-Codes
-process.stdout.write('3D Printer G-Codes...');
-const printerG = require('../src/json/printer/gcodes.json');
-valid = validate(printerG);
-
-if (valid) {
-    process.stdout.write('OK!\n\n');
-} else {
-    process.stderr.write(validate.errors);
-}
-
-// Validate 3D Printer M-Codes
-process.stdout.write('3D Printer M-Codes...');
-const printerM = require('../src/json/printer/mcodes.json');
-valid = validate(printerM);
-
-if (valid) {
-    process.stdout.write('OK!\n\n');
-} else {
-    process.stderr.write(validate.errors);
-}
+        if (valid) {
+            process.stdout.write('OK\n\n');
+        } else {
+            for (const err of validate.errors) {
+                process.stdout.write(`${err.dataPath} ${err.message ?? ''}\n`);
+            }
+            process.stdout.write(`-- ${mt} M-Codes has errors --`);
+        }
+    } catch (err) {
+        if (err instanceof Error && err.code === 'ENOENT') {
+            process.stderr.write(err.message);
+        }
+    }
+});
 
 process.stdout.write('Done! ');
 
