2265c466c4a04a28a46a80f7dc6f5c954ed39a85
Mike Centola <mcentola@appliedengdesign.com>
2021-03-20 21:54:38 -0400

    updated test file

--- src/test/test.ts
+++ src/test/test.ts
@@ -8,6 +8,9 @@ import { GReference, MachineTypes } from '../index';
 
 const g = new GReference(MachineTypes.Mill);
 
-process.stdout.write(`${g.getShortDescByCode('G00')}\n`);
-process.stdout.write(`${g.getDescByCode('G00')}\n`);
-// process.stdout.write(`${g.getDescByCode('M00')}\n`);
+process.stdout.write(`${g.getShortDesc('G00') ?? 'Cannot find G-Code'}\n`);
+process.stdout.write(`${g.getDesc('G0') ?? 'Cannot find G-Code'}\n`);
+process.stdout.write(`${g.getDesc('G00') ?? 'Cannot find G-Code'}\n`);
+process.stdout.write(`${g.getShortDesc('M00') ?? 'Cannot find M-Code'}\n`);
+process.stdout.write(`${g.getShortDesc('G1') ?? 'Cannot find G-Code'}\n`);
+console.error(g.get('G1'));
