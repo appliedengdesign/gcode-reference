96635c93e880696c701b448a3a7e585e5d5512b5
Mike Centola <mcentola@appliedengdesign.com>
2022-07-22 23:31:54 -0400

    Refactor types to fit new Schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,8 +4,9 @@
  * -------------------------------------------------------------------------------------------- */
 'use strict';
 
+import { CNCCodes } from './base';
 import { Categories, Category } from './categories';
-import { CNCCodes, Code, CodeType, CodeTypes, ICode } from './codes';
+import { Code, CodeType, CodeTypes, ICode } from './codes';
 import { MachineType, MachineTypes } from './machinetypes';
 import { Parameters } from './parameters';
 import { Variant, Variants } from './variants';
