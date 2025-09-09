b2652272ba14169c832cca1ddd3cd65facbb1f7f
Mike Centola <mcentola@appliedengdesign.com>
2025-09-09 13:42:47 -0400

    chore: update eslint config to add JSON linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,27 +3,35 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  * -------------------------------------------------------------------------------------------- */
 
-import eslint from "@eslint/js";
-import { defineConfig, globalIgnores } from "eslint/config";
-import tseslint from "typescript-eslint";
-import _import from "eslint-plugin-import";
-import prettier from "eslint-plugin-prettier/recommended";
-import globals from "globals";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import _import from 'eslint-plugin-import';
+import globals from 'globals';
+import js from '@eslint/js';
+import json from '@eslint/json';
+import prettier from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
 
 
 export default defineConfig([
+    
+    js.configs.recommended,
+    _import.flatConfigs.recommended,
+    _import.flatConfigs.typescript,
+
     globalIgnores([
         'eslint.config.mjs',
         'out/*',
         'dist/*',
     ]),
 
-    eslint.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
-    _import.flatConfigs.recommended,
-    _import.flatConfigs.typescript,
-    
     {
+        files: [
+            'src/**/*.ts',
+            'scripts/*.ts',
+            'test/*.ts',
+        ],
+
         languageOptions: {
             sourceType: 'module',
             parser: tseslint.parser,
@@ -40,13 +48,6 @@ export default defineConfig([
                 ...globals.mocha,
             }
         },
-    },
-    {
-        files: [
-            'src/**/*.ts',
-            'scripts/*.ts',
-            'test/*.ts',
-        ],
 
         rules: {
             '@typescript-eslint/naming-convention': [
@@ -86,6 +87,7 @@ export default defineConfig([
             'yoda': 'error',
         },
     },
+
     {
         // Turn off rules that will cause errors with chai
         files: [
@@ -96,85 +98,16 @@ export default defineConfig([
            'no-unused-expressions': 'off',
            '@typescript-eslint/no-unused-expressions': 'off',
         }
+    },
 
-
-        /* rules: {
-            "@typescript-eslint/explicit-module-boundary-types": "off",
-            "@typescript-eslint/indent": "off",
-
-            "@typescript-eslint/no-empty-function": ["warn", {
-                allow: ["constructors"],
-            }],
-
-            "@typescript-eslint/no-empty-interface": "error",
-            "@typescript-eslint/no-explicit-any": "off",
-
-            "@typescript-eslint/no-inferrable-types": ["warn", {
-                ignoreParameters: true,
-                ignoreProperties: true,
-            }],
-
-            "@typescript-eslint/no-namespace": "off",
-            "@typescript-eslint/no-non-null-assertion": "off",
-
-            "@typescript-eslint/no-unused-vars": ["warn", {
-                args: "after-used",
-                argsIgnorePattern: "^_",
-                ignoreRestSiblings: true,
-                varsIgnorePattern: "^_$",
-            }],
-
-            "@typescript-eslint/no-use-before-define": "error",
-            "@typescript-eslint/semi": "error",
-            "@typescript-eslint/unbound-method": "off",
-            "arrow-parens": ["error", "as-needed"],
-
-            "brace-style": ["warn", "1tbs", {
-                allowSingleLine: true,
-            }],
-
-            "comma-dangle": ["error", "only-multiline"],
-            complexity: "off",
-            "dot-notation": "error",
-            eqeqeq: ["error", "smart"],
-            "eol-last": "error",
-            "import/no-dynamic-require": "error",
-            "import/no-default-export": "error",
-            "import/no-duplicates": "error",
-            "import/no-self-import": "error",
-
-            "import/no-unresolved": ["warn", {
-                ignore: ["vscode"],
-            }],
-
-            "max-classes-per-file": ["error", 1],
-
-            "no-bitwise": "error",
-            "no-console": "off",
-            
-            "no-inner-declarations": "off",
-            "no-invalid-this": "error",
-            "no-trailing-spaces": "error",
-            "no-var": "error",
-            "prefer-arrow-callback": "error",
-            "prefer-const": "error",
-            "prefer-numeric-literals": "error",
-            "prefer-object-spread": "error",
-            "prefer-rest-params": "error",
-            "prefer-spread": "error",
-            "prefer-template": "error",
-            "prettier/prettier": "error",
-            semi: ["error", "always"],
-            "semi-style": ["error", "last"],
-            "spaced-comment": ["error", "always"],
-            "space-in-parens": ["error", "never"],
-
-            'sort-imports': ['error', {
-                ignoreCase: true,
-                ignoreMemberSort: false,
-            }],
-
-        } */
+    // Lint JSON
+    {
+        files: ['/**/*.json'],
+        ignores: ['package-lock.json'],
+        plugins: { json },
+        language: 'json/json',
+        ...json.configs.recommended,
+        extends: [ tseslint.configs.disableTypeChecked],   
     },
 
     prettier,
